0c68cf1b119b0b913ee99b21b410219c1b00e560
akash <akash@LAPTOP-IT8S7NC3>
2022-10-23 19:08:11 +0530

    First Commit

---
+++ BATCHM26/src/com/cg/corejava/DisplayOutput.java
@@ -0,0 +1,10 @@
+package com.cg.corejava;
+
+public class DisplayOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hellow");
+	}
+
+}
---
+++ BATCHM26/src/com/cg/corejava/MultiplicationProgram.java
@@ -0,0 +1,19 @@
+package com.cg.corejava;
+
+import java.util.Scanner;
+
+public class MultiplicationProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter First Number ");
+		int num1=obj.nextInt();
+		System.out.println("Enter second Number ");
+		int num2=obj.nextInt();
+		int num3=num1+num2;
+		System.out.println("Multiplication Of Two Number Is "+num3);
+
+	}
+
+}
