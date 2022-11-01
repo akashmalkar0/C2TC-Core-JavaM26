4022c73d353b488582db482706f11c8078222969
akash <akash@LAPTOP-IT8S7NC3>
2022-11-01 14:04:13 +0530

    New Commit

---
+++ BATCHM26/src/com/cg/corejava/StringExample.java
@@ -0,0 +1,16 @@
+package com.cg.corejava;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Akash";  
+		char ch[]={'S','a','n','g','r','a','m'};    
+		String s2=new String(ch);
+		String s3=new String("Sagar"); 
+		System.out.println(s1);    
+		System.out.println(s2);    
+		System.out.println(s3);
+	}
+
+}
